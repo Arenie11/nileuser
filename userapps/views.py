a340838e271f7f75f7e76fce55924240527b7031
Zainab Alugo <zainabalugo@gmail.com>
2024-12-06 13:22:09 +0100

    views

--- userapps/views.py
+++ userapps/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework import Response
+from rest_framework.views import APIView
+from rest_framework import serializers
+from . serializers import UserSerializer, ProfileSerializer, RegistrationSerializer
+from . models import Profile
+from django.contrib.auth.models import User
+#registration
+class RegistrationView(APIView):
+    def post (self, request):
+        try:
+           serializer= RegistrationSerializer(data= request.data)
+           if serializer.is_valid():
+               serializer.save()
+               return Response(serializer.data, status=status.HTTP_201_CREATED)
+           return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+           
+        except Exception as e:
+            return Response({'error': str(e)}, status= status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 # Create your views here.
